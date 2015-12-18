15954578774144877c214686c80f6606a4cff1e4
Allison Nelson <allison.nelson13@gmail.com>
2015-12-18 10:57:18 -0800

    let's do it live

--- bot.js
+++ bot.js
@@ -23,13 +23,13 @@ console.log(tweet);
 var Twit = require('twit');
 
 
-//var T = new Twit(
-//{
-//    consumer_key:         process.env.CONSUMER_KEY
-//  , consumer_secret:      process.env.CONSUMER_SECRET
-//  , access_token:         process.env.ACCESS_TOKEN
-//  , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
-//}
-//);
+var T = new Twit(
+{
+    consumer_key:         process.env.CONSUMER_KEY
+  , consumer_secret:      process.env.CONSUMER_SECRET
+  , access_token:         process.env.ACCESS_TOKEN
+  , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+}
+);
 
-//T.post('statuses/update', { status: tweet }, function(err, data, response) {  console.log(err)})
+T.post('statuses/update', { status: tweet }, function(err, data, response) {  console.log(err)})
